63da9c63caac87ccd7e7505cad10273068dc1c9e
logonrm <logonrm@fiap.com.br>
2018-01-29 23:07:23 -0200

    Initial commit

---
+++ app/src/main/java/com/example/ocimara/listacomasynctask/listener/OnItemClickListener.java
@@ -0,0 +1,10 @@
+package com.example.ocimara.listacomasynctask.listener;
+import android.view.View;
+
+public interface OnItemClickListener {
+        void onClick(View view, int position);
+
+
+
+
+}
